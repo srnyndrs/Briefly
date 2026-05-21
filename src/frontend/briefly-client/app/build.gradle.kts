7b7fe30622c52d5e8125b619479f38448ca6b0a5
Surányi András <srnyndrs@gmail.com>
2026-05-21 19:21:59 +0200

    implement ui update

--- src/frontend/briefly-client/app/build.gradle.kts
+++ src/frontend/briefly-client/app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.browser)
+    implementation(libs.androidx.compose.ui.geometry)
     // Dagger Hilt
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.android)
--- src/frontend/briefly-client/app/src/main/java/com/srnyndrs/android/briefly/ui/screen/content/screen/feed_details/FeedDetailsState.kt
+++ src/frontend/briefly-client/app/src/main/java/com/srnyndrs/android/briefly/ui/screen/content/screen/feed_details/FeedDetailsState.kt
@@ -1,9 +1,11 @@
 package com.srnyndrs.android.briefly.ui.screen.content.screen.feed_details
 
+import com.srnyndrs.android.briefly.domain.model.content.ArticleItem
+import com.srnyndrs.android.briefly.domain.model.content.ArticlePagingResult
 import com.srnyndrs.android.briefly.domain.model.content.FeedSourceDetails
 import com.srnyndrs.android.briefly.ui.model.UiState
 
 data class FeedDetailsState(
     val feedDetails: UiState<FeedSourceDetails> = UiState.Idle,
-    val articles: UiState<String> = UiState.Idle,
+    val articles: UiState<List<ArticleItem>> = UiState.Idle,
 )
